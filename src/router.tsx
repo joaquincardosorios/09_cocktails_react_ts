a954e2cd5b27dbb686059ad072452097cf493f87
Joaquin Cardoso <joaquin.cardoso.r@gmail.com>
2025-01-16 22:54:03 +0800

    feature/optimize router

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,26 @@
+import {lazy, Suspense} from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import IndexPage from './views/IndexPage'
-import FavoritePages from './views/FavoritePages'
 import Layout from './layouts/Layout'
+
+// import IndexPage from './views/IndexPage'
+const IndexPage = lazy( () => import('./views/IndexPage'))
+const FavoritePages = lazy( () => import('./views/FavoritePages'))
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />} >
-          <Route path='/' element={<IndexPage/>} index />
-          <Route path='/favoritos' element={<FavoritePages/>} />
+          <Route path='/' element={
+            <Suspense fallback='Cargando...'>
+              <IndexPage/>
+            </Suspense>
+          } index />
+          <Route path='/favoritos' element={
+            <Suspense fallback='Cargando...'>
+              <FavoritePages />
+            </Suspense>
+          } />
         </Route>
       </Routes>
     </BrowserRouter>
